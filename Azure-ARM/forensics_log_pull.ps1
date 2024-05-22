bab77589b5b1ad3eef2f62ed8566017e5bf7bd48
Naveen Angali <Naveen.angali@profisee.com>
2024-05-22 15:47:10 -0400

    updated  forensics script to account for multiple log files

--- Azure-ARM/forensics_log_pull.ps1
+++ Azure-ARM/forensics_log_pull.ps1
@@ -1,20 +1,28 @@
 #Pull Product Services, IIS, Event Viewer logs as well as Netstat and TCPConnection logs
 $DT = get-date -Format "ddd-MM-dd-yy-HHmmss-ffff-Z" 
-mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Config" 
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Gateway"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Attachments"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Auth"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Governance"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Monolith"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Workflows"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Web"
+mkdir "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Webportal"
 mkdir "$env:TEMP\all-Logs\$DT\EventViewerLogs"
 mkdir "$env:TEMP\all-Logs\$DT\TCPLogs"
 mkdir "$env:TEMP\all-Logs\$DT\IISLogs"
-copy "$env:SystemRoot\System32\winevt\Logs\*" "$env:TEMP\all-Logs\$DT\EventViewerLogs\"
-copy c:\profisee\configuration\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\config-log.log
-copy c:\profisee\gateway\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\gateway-log.log
-copy c:\profisee\services\attachments\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\attachments-log.log
-copy c:\profisee\services\auth\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\auth-log.log
-copy c:\profisee\services\governance\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\governance-log.log
-copy c:\profisee\services\monolith\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\monolith-log.log
-copy c:\profisee\services\workflows\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\workflows-log.log
-copy c:\profisee\web\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\web-log.log
-copy c:\profisee\webportal\logfiles\systemlog.log $env:TEMP\all-Logs\$DT\ProfiseeLogs\webportal-log.log
-copy C:\inetpub\logs\LogFiles\W3SVC1\*.log $env:TEMP\all-Logs\$DT\IISLogs\
+robocopy "$env:SystemRoot\System32\winevt\Logs\" "$env:TEMP\all-Logs\$DT\EventViewerLogs" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\configuration\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Config" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\gateway\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Gateway" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\services\attachments\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Attachments" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\services\auth\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Auth" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\services\governance\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Governance" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\services\monolith\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Monolith" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\services\workflows\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Workflows" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\web\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Web" /E /COPYALL /DCOPY:T
+robocopy "c:\profisee\webportal\logfiles" "$env:TEMP\all-Logs\$DT\ProfiseeLogs\Webportal" /E /COPYALL /DCOPY:T
+robocopy "c:\inetpub\logs\LogFiles\W3SVC1" "$env:TEMP\all-Logs\$DT\IISLogs" /E /COPYALL /DCOPY:T
 netstat -anobq > $env:TEMP\all-Logs\$DT\TCPLogs\netstat.txt
 Get-NetTCPConnection | Group-Object -Property State, OwningProcess | Select -Property Count, Name, @{Name="ProcessName";Expression={(Get-Process -PID ($_.Name.Split(',')[-1].Trim(' '))).Name}}, Group | Sort Count -Descending | out-file $env:TEMP\all-Logs\$DT\TCPLogs\TCPconnections.txt
 
